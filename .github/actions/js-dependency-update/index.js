9a0e115b133847d701ce0e371fa6eb153a2dbd32
Everton Confortin <351672+evertonlc@users.noreply.github.com>
2026-03-15 16:28:18 -0300

    doc: Steps for implementing the dependency update check

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,6 +1,19 @@
 const core = require('@actions/core');
  
-async function run() { 
+async function run() {
+  /*
+  1. Parse inputs: 
+    1.1 base-branch from which check for updates
+    1.2 target-branch to use to create the PR
+    1.3 Github Token for authentication purposes (to create PRs)
+    1.4 Working directory for which to check for dependencies
+  2. Execute the npm update command withing the working directory
+  3. Check wheter there are changes in the package*.json files
+  4. If there are modified files:
+    4.1 Add and commit files to the base-branch
+    4.2 Create a PR to the base-branch using the octokit API
+  5. Otherswise, conclude the custom action
+  */
   core.info('I am a custom JS action');
 }
  
