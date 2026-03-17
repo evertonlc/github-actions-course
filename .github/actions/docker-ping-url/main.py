7649cb79c2a9d18e8d58d803ba424c2d152147cd
Everton Confortin <351672+evertonlc@users.noreply.github.com>
2026-03-16 22:15:32 -0300

    feat: Python ping

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,33 @@
+import requests
+import time
+import os
+
+def ping_url(url: str, max_trials: int, delay_seconds: int) -> bool:
+
+    for _ in range(max_trials):
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                return True
+        except requests.RequestException:
+            pass
+        time.sleep(delay_seconds)
+    return False
+
+def run():
+
+    url = os.getenv("INPUT_URL")
+    max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
+    delay_seconds = int(os.getenv("INPUT_DELAY_SECONDS"))
+
+    if not url:
+        print("URL environment variable is not set.")
+        return
+
+    if ping_url(url, max_trials, delay_seconds):
+        print(f"Successfully pinged {url}")
+    else:
+        exit(f"Failed to ping {url} after {max_trials} attempts.")
+
 if __name__ == "__main__":
-    print("Hello world")
+    run()
